a7649eaa7f7b1b1b317053ec00daaed4efa0cfa3
samghadri <sam.ghadri@gmail.com>
2017-11-22 20:21:35 +0000

    membersPostsModel

--- holidayshare/groups/models.py
+++ holidayshare/groups/models.py
@@ -7,6 +7,28 @@ User = get_user_model()
 from django import template
 register = template.Library()
 
+class Group(models.Model):
+    name = models.CharField(max_length=40,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    country = models.CharField(max_length=40,unique = True)
+    description = models.TextField(blank=True)
+    description_html = models.TextField(editable=False, blank=True)
+    members = models.ManyToManyField(User,through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args,**kwargs)
+    def get_absolute_url(self):
+        return reverse('members:single',kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering =['name']
+
+
 class GroupMember(models.Model):
     group = models.ForeignKey(Group,related_name='membership')
     user = models.ForeignKey(User,related_name='user_members')
