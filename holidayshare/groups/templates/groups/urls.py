7f9088fd01b5898864d940e6044ae0838da49881
samghadri <sam.ghadri@gmail.com>
2017-11-23 22:36:29 +0000

    register group to admin

---
+++ holidayshare/groups/templates/groups/urls.py
@@ -0,0 +1,12 @@
+#Groups url
+from django.conf.urls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns=[
+    url(r'^$',views.ListGroups.as_view(),name='all'),
+    url(r'^create/$',views.BuildGroup.as_view(),name='create'),
+    url(r'posts/in/(?P<slug>[-/w]+)/$',views.SingleGroup.as_view(),name='single'),
+
+]
