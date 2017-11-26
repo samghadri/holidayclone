73b270b86eb461deeec338b307059d07df0f9b0d
samghadri <sam.ghadri@gmail.com>
2017-11-26 19:09:09 +0000

    starting PostViews

--- holidayshare/groups/admin.py
+++ holidayshare/groups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
+
 
 class GroupMemberInline(admin.TabularInline):
     model = models.GroupMember
--- holidayshare/posts/views.py
+++ holidayshare/posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from django.views import generic
+django django.http import Http404
+from braces.views import SelectRelatedMixin
+from . import models
+from . import forms
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your views here.
+class PostList(SelectRelatedMixin, generic.ListView):
+
+    model = models.Post
+    select_related = ('user','group')
+    
