12beeed0cf9861374ffbf41130c258064489dec2
samghadri <sam.ghadri@gmail.com>
2017-11-27 12:30:16 +0000

    posts views

--- holidayshare/posts/views.py
+++ holidayshare/posts/views.py
@@ -13,4 +13,17 @@ class PostList(SelectRelatedMixin, generic.ListView):
 
     model = models.Post
     select_related = ('user','group')
-    
+
+class UserPost(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+
+        except User.DoestNotExist:
+            raise Http404
+
+        else:
+            return self.post_user.posts.all()
